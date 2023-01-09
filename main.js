4e2a530f266a1dc8d872f2fdc673ebfaed56901c
Adeola <adeolaweb@gmail.com>
2023-01-09 18:52:54 +0100

    go

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const markAsRead = document.querySelector("header > p");
 const notificationCount = document.querySelector(".notif-count");
 const activeClass = document.querySelectorAll(".active");
+const notifications = document.querySelectorAll(".notif");
 
+//To mark all as read.
 markAsRead.addEventListener("click", () => {
   notificationCount.textContent = "0";
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < activeClass.length; i++) {
     activeClass[i].classList.remove("active");
   }
+});
+
+//Dynamic notifications.
+const notifArr = Array.from(notifications);
+notifArr.forEach((notif) => {
+  notif.addEventListener("click", () => {
+    //To toggle active Classes
+    notif.classList.toggle("active");
+
+    //To increase notification Count.
+    const activeClasses = Array.from(document.getElementsByClassName("active"));
+    notificationCount.textContent = activeClasses.length;
+  });
 });
